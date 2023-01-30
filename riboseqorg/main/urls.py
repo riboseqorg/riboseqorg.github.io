4685110ebe3b1f5b8f725970d32bbbe7dd06cca6
Jack Tierney <jack.tierney@student.ucc.ie>
2023-01-30 15:32:24 +0000

    start query search

--- riboseqorg/main/urls.py
+++ riboseqorg/main/urls.py
@@ -1,9 +1,12 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
+
+from .views import OrderListJson
 
 urlpatterns = [
     path('', views.index, name='home'),
     path('home', views.index, name='home'),
     path('db', views.db, name='db'),
     path('add', views.add, name='add'),
+    re_path(r'^my/datatable/data/$', OrderListJson.as_view(), name='order_list_json'),
 ]
--- riboseqorg/main/views.py
+++ riboseqorg/main/views.py
@@ -1,7 +1,59 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
+
 from django.http import HttpResponseRedirect
 from .models import Data, Sample
 from .forms import addNewSample
+
+from django_datatables_view.base_datatable_view import BaseDatatableView
+from django.utils.html import escape
+
+
+class OrderListJson(BaseDatatableView):
+    # The model we're going to show
+    model = Data
+
+    # define the columns that will be returned
+    columns = ['sample_name', 'sample_description', 'cell_line', 'treatment', 'library_type']
+
+    # define column names that will be used in sorting
+    # order is important and should be same as order of columns
+    # displayed by datatables. For non-sortable columns use empty
+    # value like ''
+    order_columns = ['sample_name', 'sample_description', 'cell_line', 'treatment', 'library_type']
+
+    # set max limit of records returned, this is used to protect our site if someone tries to attack our site
+    # and make it return huge amount of data
+    max_display_length = 500
+
+    def render_column(self, row, column):
+        # We want to render user as a custom column
+        if column == 'user':
+            # escape HTML for security reasons
+            return escape('{0} {1}'.format(row.customer_firstname, row.customer_lastname))
+        else:
+            return super(OrderListJson, self).render_column(row, column)
+
+    def filter_queryset(self, qs):
+        # use parameters passed in GET request to filter queryset
+
+        # simple example:
+        search = self.request.GET.get('search[value]', None)
+        if search:
+            qs = qs.filter(name__istartswith=search)
+
+        # more advanced example using extra parameters
+        filter_customer = self.request.GET.get('customer', None)
+
+        if filter_customer:
+            customer_parts = filter_customer.split(' ')
+            qs_params = None
+            for part in customer_parts:
+                q = Q(customer_firstname__istartswith=part) | Q(customer_lastname__istartswith=part)
+                qs_params = qs_params | q if qs_params else q
+            qs = qs.filter(qs_params)
+        return qs
+
 
 # Create your views here.
 def index(response):
@@ -10,6 +62,11 @@ def index(response):
 
 def db(response):
     ls = Data.objects.get(id=1)
+    paginator = Paginator(contact_list, 25) # Show 25 contacts per page.
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'list.html', {'page_obj': page_obj})
     return render(response, "main/database.html", {'ls': ls })
 
 def add(response):
